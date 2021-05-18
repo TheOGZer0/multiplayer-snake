8bda2a956f0591429eb4a5d5c556630bda26179e
CorporateGreed <jim12.brood@gmail.com>
2021-05-18 02:20:10 +0200

    Add key listener to game.py

--- src/game/game.py
+++ src/game/game.py
@@ -1,9 +1,9 @@
+import pynput.keyboard
+
 from src.user.user import BaseUser
 from src.ui.interface import Interface
 
 import time
-
-from pynput.keyboard import Key, Controller
 
 
 class Game:
@@ -21,6 +21,12 @@ class Game:
         self.players = players
         self.interface = interface
         self.frames_per_second = frames_per_second
+
+        self.key_listener = pynput.keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release
+        )
+        self.key_listener.start()
 
     def play(self, wait: bool = False) -> None:
         """Event loop of the game, returns None
@@ -40,5 +46,26 @@ class Game:
 
         The code for the game should be added to this method. Do not edit the play() method as it is only a wrapper
         for this method.
+        """
+        return None
+
+    def on_press(self, key) -> None:
+        """Shell for on press events, returns None
+
+        This method can be inherited to fit the need of any game. Fires every time a key is pressed.
+
+        Attributes:
+            key: Required by pynput and automatically passed on.
+        """
+        print(key)
+        return None
+
+    def on_release(self, key) -> None:
+        """Shell for on release events, returns None
+
+        This method can be inherited to fit the need of any game. Fires every time a key is released.
+
+        Attributes:
+            key: Required by pynput and automatically passed on.
         """
         return None
